ba79b74879965ca6c378a3aa52e4c44c4c00769d
Akshat Gupta <akshat41121995@gmail.com>
2017-05-19 12:12:51 +0530

    account and layout changes

--- home.php
+++ home.php
@@ -2,16 +2,7 @@
 <?php 
 include 'includes/connection.inc.php';
 include 'includes/layout.inc.php';?>
-<body>
-<div class="container">
-<div class="row">
-<div class="col-lg-6 xyz" style="padding-left: 50px; margin-top: 20px;">
-<h2">Search for the location</h2>
-    <div class="input-group" style="padding-left: 100px;">
-      <input type="text" class="form-control" placeholder="Search for location">
-        <button class="btn btn-success" type="button">Go!</button>
-</div></div></div></div>
-</body>
+<link rel="stylesheet" type="text/css" href="includes/bs.min.css">
 <?php include 'includes/footer.inc.php'; ?>
 
 
