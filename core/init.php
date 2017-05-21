ddecbd41c7b10a82ad4e493fec73b9a8212d8563
Akshat Gupta <akshat41121995@gmail.com>
2017-05-22 00:06:59 +0530

    OOP based DB class started

--- core/init.php
+++ core/init.php
@@ -4,7 +4,7 @@ session_start();
 $GLOBALS['config']= array(
 	'mysql'=>array(
 		'host'=>'127.0.0.1',
-		'username'='root',
+		'username'=>  'root',
 		'password'=>'',
 		'db'=>'swqms'
 
--- forgot.php
+++
@@ -1,2 +0,0 @@
-<h2>Forgot Password</h2>
-<h3>Recover from forgot password</h3>
