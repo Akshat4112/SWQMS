bdebbb4e1a41e9d06bf810fdc151eedf4a57451f
Akshat Gupta <akshat41121995@gmail.com>
2017-05-23 01:42:58 +0530

    Unnecessary files deleted

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 require_once 'core/init.php';
 
-DB::getInstance();
+$user= DB::getInstance("SELECT username FROM users WHERE  username = ?",array('alex'));
+if($user->error()){
+	echo 'No user';
+}else{
+	echo 'OK';
+}
+
